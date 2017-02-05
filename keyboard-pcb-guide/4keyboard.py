ea6796fd18dbc66112452cd87503f5513ccdfad8
Jared <g42-github.com@gits.org>
2017-02-05 15:59:52 -0500

    wired up the keyboard, can be any size up to 8x8 if I want right now

--- keyboard-pcb-guide/4keyboard.py
+++ keyboard-pcb-guide/4keyboard.py
@@ -1,10 +1,22 @@
 #Ensure that KISYSMOD is set, on ubuntu standard install use KISYSMOD=/usr/share/kicad/modules
 #Let's start with getting the SKiDL library
 from skidl import *
+from os.path import dirname, realpath, sep
+
+#We need to import the kicad libraries we have as git submodules
+#skidl.lib_search_paths_kicad.append(u'./kicad-libraries/kicad_lib_tmk/')
+keyboardParts_lib = dirname(realpath(__file__)) + sep + 'kicad-libraries/kicad_lib_tmk/keyboard_parts.lib'
 
 #From the skidl docs we will create some templates, edited to not do the footprints yet, but those will be added
 res = Part('device', 'R', dest=TEMPLATE)
 cap = Part('device', 'C', dest=TEMPLATE)
+dio = Part('device', 'D', dest=TEMPLATE)
+key = Part(keyboardParts_lib, '\~KEYSW', dest=TEMPLATE)
+
+#We create a function to return a button with diode
+#@SubCircuit
+#def keyboard_key():
+	
 
 #create the global nets up gront
 gnd = Net('GND')
@@ -12,18 +24,15 @@ gnd.drive = POWER
 vcc = Net('+5V')
 vcc.drive = POWER
 
-#We need to import the kicad libraries we have as git submodules
-skidl.lib_search_paths_kicad.append(u'./kicad-libraries/kicad_lib_tmk/')
-#keyboardParts_lib = r'./kicad-libraries/kicad_lib_tmk/keyboard_parts.lib'
-
 #The first component from the howto is the ATMEGA32U4, let's add that
 atmega = Part('atmel', 'ATmega32U4')	#TODO footprint
+atmega[:] += NC	#we connect all the pins to the NC net, which is overwritten if we use the pin
 
 #add in the timing circuit
-crystal = Part('device', 'Crystal')
+crystal = Part(keyboardParts_lib, 'XTAL_GND')
 crystal_decouple = cap(2, value='22pf')
 #connect these all up
-crystal[1,2] += atmega['XTAL1', 'XTAL2']
+crystal[1,2,3] += atmega['XTAL1', 'XTAL2'], gnd
 crystal.ref = 'X1'
 crystal_decouple[0][1,2] += crystal[1], gnd
 crystal_decouple[1][1,2] += crystal[2], gnd
@@ -44,11 +53,11 @@ reset_sw[1,2] += gnd, atmega['RESET']
 reset_res[1][1,2] += gnd, atmega[33]	#I use the number here because the name sucks for this
 
 #Add a USB port
-usb_conn = Part('conn', 'USB_OTG')
+usb_conn = Part(keyboardParts_lib, '\~USB_mini_micro_B')
 usb_res = res(2, value='22')
 usb_cap = cap(value='1uf')
 #connect it all up
-usb_conn['VCC'] += vcc, atmega['UVCC']
+usb_conn['VUSB'] += vcc, atmega['UVCC']
 usb_res[0][1,2] += usb_conn['D-'], atmega['D-']
 usb_res[1][1,2] += usb_conn['D\+'], atmega['D\+']
 usb_conn['ID'] += NC	#not connected
@@ -61,13 +70,38 @@ atmega['VBUS', 'VCC', 'AVCC'] += vcc
 atmega['GND'] += gnd	#this will select all 'GND' pins
 
 
+#We start into the 4 keys for this version
+#TODO: generalize this to any size
+x = 2
+y = 2
+keys = [None] * x
+keys_dio = [None] * x
+for i in range(0, x):
+	#We create a pair of two dimensional arrays of key switches and diodes for the keybaord
+	keys[i] = key(y)
+	keys_dio[i] = dio(y)
+drive = Bus('KEY_Drive', x)	#Create the nets in a bus for the drive side
+drain = Bus('Key_Drain', y)	#Create the nets in a bus for the drain side
+for ix in range(0, x):
+	for iy in range(0, y):
+		keys[ix][iy][2] += keys_dio[ix][iy][2]	#Hook the diodes up to the keys
+		keys[ix][iy][1] += drive[ix]		#Hook up the drive bus to the keys input
+		keys_dio[ix][iy][1] += drain[iy]	#Hook up the drain to the other end of the diode
+
+#In a slight change from the tutorial source, I will be connecting the drive(col) to PB? and drain(row) to PD?
+#It allows us to grow the keyboard up to 8 x 8 = 64 keys in this code
+for ix in range(0, x):
+	drive[ix] += atmega['.*PB' + str(ix) + '.*']
+for iy in range(0, y):
+	drain[iy] += atmega['.*PD' + str(iy) + '.*']
 
 
 
 
 
 
-
+#test it
+ERC()
 
 #print it out
 generate_netlist(sys.stdout)
